9a762566e7fcbed2f9dad7cc710c2aa068f4636f
PR202111 <prashant_b240272cs@nitc.ac.in>
2025-06-24 15:46:41 +0530

    Day 3 Simple calculator

---
+++ day3/simple_calculator/target/debug/deps/simple_calculator-6671e8cc1eb88ddc.d
@@ -0,0 +1,5 @@
+/Users/pashantraj/Desktop/100_days/rust/day3/simple_calculator/target/debug/deps/simple_calculator-6671e8cc1eb88ddc: src/main.rs
+
+/Users/pashantraj/Desktop/100_days/rust/day3/simple_calculator/target/debug/deps/simple_calculator-6671e8cc1eb88ddc.d: src/main.rs
+
+src/main.rs:
---
+++ day3/simple_calculator/target/debug/simple_calculator.d
@@ -0,0 +1,1 @@
+/Users/pashantraj/Desktop/100_days/rust/day3/simple_calculator/target/debug/simple_calculator: /Users/pashantraj/Desktop/100_days/rust/day3/simple_calculator/src/main.rs
